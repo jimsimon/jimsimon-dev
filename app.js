6d2c47a5bee79e40062f6e0332cf1632ab747baa
jimsimon <jim.j.simon@gmail.com>
2016-01-16 21:18:07 -0500

    Setup api and static resource routes

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ var finalhandler = require('finalhandler');
 var fs = require('fs');
 var http2 = require('http2');
 var Router = require('router');
+var serveStatic = require('serve-static');
+var api = require("./routes/api");
 
 var router = new Router();
-router.get('/', function (req, res) {
-    console.log(req.headers);
+var staticHandler = serveStatic('public', {'index': ['index.html']});
 
-    res.setHeader('Content-type', 'text/html');
-    return res.end('<h1>Hello, Secure World!</h1>');
-});
+router.use(staticHandler);
+router.use('/api', api);
 
 var options = {
     key: fs.readFileSync('./certificate/localhost.key'),
@@ -21,5 +21,6 @@ var port = process.env.VCAP_APP_PORT || 8080;
 http2.createServer(options, app).listen(port);
 
 function app(req, res) {
-    router(req, res, finalhandler(req, res));
+    var done = finalhandler(req, res);
+    router(req, res, done);
 }
