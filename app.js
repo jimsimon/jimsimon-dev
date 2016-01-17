2761b98cc7b54d37a283b0ddedcc28ca9ab78c0d
jimsimon <jim.j.simon@gmail.com>
2016-01-17 02:36:21 -0500

    Switched to https and added initial page layout

--- app.js
+++ app.js
@@ -2,14 +2,16 @@
 
 var finalhandler = require('finalhandler');
 var fs = require('fs');
-var http2 = require('http2');
+var https = require('https');
 var Router = require('router');
 var serveStatic = require('serve-static');
 var api = require("./routes/api");
+var compression = require('compression');
 
-var router = new Router();
+var router = new Router({});
 var staticHandler = serveStatic('public', {'index': ['index.html']});
 
+router.use(compression({}));
 router.use(staticHandler);
 router.use('/api', api);
 
@@ -18,7 +20,7 @@ var options = {
     cert: fs.readFileSync('./certificate/localhost.crt')
 };
 var port = process.env.VCAP_APP_PORT || 8080;
-http2.createServer(options, app).listen(port);
+https.createServer(options, app).listen(port);
 
 function app(req, res) {
     var done = finalhandler(req, res);
---
+++ public/load-polyfills.js
@@ -0,0 +1,11 @@
+var webComponentsSupported =
+    'registerElement' in document &&
+    'import' in document.createElement('link') &&
+    'content' in document.createElement('template');
+
+if (!webComponentsSupported) {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'bower_components/webcomponentsjs/webcomponents-lite.min.js';
+    document.head.appendChild(script);
+}
